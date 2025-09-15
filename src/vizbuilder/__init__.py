0732dd6746b8288bf0730f00de84ebf572fe75b9
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-09-15 21:27:00 +0200

    wip  manual re-implementation of plotly express arguments

--- src/vizbuilder/__init__.py
+++ src/vizbuilder/__init__.py
@@ -1,17 +1,10 @@
-from plotly.express.colors import cyclical, diverging, qualitative, sequential
-
 from ._lib import Displayer
-from ._scales import extract_scales, show_scales
+from ._scales import show_scales
 from ._types import Templates, TemplatesValues
 
 __all__ = [
     "show_scales",
     "Displayer",
-    "extract_scales",
-    "sequential",
-    "cyclical",
-    "qualitative",
-    "diverging",
     "Templates",
     "TemplatesValues",
 ]
