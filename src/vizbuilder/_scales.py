6c0252cb6fcad51dcc25cf4fd8fb213b3b98ce4c
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-09-16 02:08:02 +0200

    deleted manual palettes dict, added pipeline function to directly get the palettes from the module, filter the no hex values ones

--- src/vizbuilder/_scales.py
+++ src/vizbuilder/_scales.py
@@ -1,6 +1,8 @@
 from typing import Literal, Protocol
 
 import plotly.graph_objects as go
+import polars as pl
+import pychain as pc
 from plotly.express.colors import cyclical, qualitative, sequential
 
 
@@ -8,102 +10,12 @@ class Swatchable(Protocol):
     def swatches(self) -> go.Figure: ...
 
 
-Modules = Literal["sequential", "cyclical", "qualitative"]
-
 MODULES: dict[str, Swatchable] = {
     "sequential": sequential,
     "cyclical": cyclical,
     "qualitative": qualitative,
 }
-
-PALETTES: dict[str, list[str]] = {
-    "Cividis": sequential.Cividis,
-    "Cividis_r": sequential.Cividis_r,
-    "Inferno": sequential.Inferno,
-    "Inferno_r": sequential.Inferno_r,
-    "Magma": sequential.Magma,
-    "Magma_r": sequential.Magma_r,
-    "Plasma": sequential.Plasma,
-    "Plasma_r": sequential.Plasma_r,
-    "Plotly3": sequential.Plotly3,
-    "Plotly3_r": sequential.Plotly3_r,
-    "Turbo": sequential.Turbo,
-    "Turbo_r": sequential.Turbo_r,
-    "Viridis": sequential.Viridis,
-    "Viridis_r": sequential.Viridis_r,
-    "Edge": cyclical.Edge,
-    "Edge_r": cyclical.Edge_r,
-    "HSV": cyclical.HSV,
-    "HSV_r": cyclical.HSV_r,
-    "IceFire": cyclical.IceFire,
-    "IceFire_r": cyclical.IceFire_r,
-    "Twilight": cyclical.Twilight,
-    "Twilight_r": cyclical.Twilight_r,
-    "mrybm": cyclical.mrybm,
-    "mrybm_r": cyclical.mrybm_r,
-    "mygbm": cyclical.mygbm,
-    "mygbm_r": cyclical.mygbm_r,
-    "Alphabet": qualitative.Alphabet,
-    "Alphabet_r": qualitative.Alphabet_r,
-    "D3": qualitative.D3,
-    "D3_r": qualitative.D3_r,
-    "Dark24": qualitative.Dark24,
-    "Dark24_r": qualitative.Dark24_r,
-    "G10": qualitative.G10,
-    "G10_r": qualitative.G10_r,
-    "Light24": qualitative.Light24,
-    "Light24_r": qualitative.Light24_r,
-    "Plotly": qualitative.Plotly,
-    "Plotly_r": qualitative.Plotly_r,
-    "T10": qualitative.T10,
-    "T10_r": qualitative.T10_r,
-}
-Palettes = Literal[
-    "Cividis",
-    "Cividis_r",
-    "Inferno",
-    "Inferno_r",
-    "Magma",
-    "Magma_r",
-    "Plasma",
-    "Plasma_r",
-    "Plotly3",
-    "Plotly3_r",
-    "Turbo",
-    "Turbo_r",
-    "Viridis",
-    "Viridis_r",
-    "Edge",
-    "Edge_r",
-    "HSV",
-    "HSV_r",
-    "IceFire",
-    "IceFire_r",
-    "Twilight",
-    "Twilight_r",
-    "mrybm",
-    "mrybm_r",
-    "mygbm",
-    "mygbm_r",
-    "Alphabet",
-    "Alphabet_r",
-    "D3",
-    "D3_r",
-    "Dark24",
-    "Dark24_r",
-    "G10",
-    "G10_r",
-    "Light24",
-    "Light24_r",
-    "Plotly",
-    "Plotly_r",
-    "T10",
-    "T10_r",
-]
-
-
-def get_palette(palette: Palettes) -> list[str]:
-    return PALETTES[palette]
+Modules = Literal["sequential", "cyclical", "qualitative"]
 
 
 def show_scales(module: Modules) -> go.Figure:
@@ -121,3 +33,77 @@ def show_scales(module: Modules) -> go.Figure:
             paper_bgcolor="#181c1a",
         )
     )
+
+
+def get_palettes() -> dict[str, list[str]]:
+    df: pl.DataFrame = (
+        pc.Iter(MODULES.values())
+        .map(
+            lambda mod: pc.Dict(mod.__dict__)
+            .filter_values(lambda v: isinstance(v, list))
+            .unwrap()
+        )
+        .pipe_into(pl.LazyFrame)
+        .unpivot(value_name="color", variable_name="scale")
+        .drop_nulls()
+        .filter(
+            pl.col("color")
+            .list.eval(pl.element().first().str.starts_with("#").alias("is_hex"))
+            .list.first()
+        )
+        .sort("scale")
+        .collect()
+    )
+
+    return (
+        pc.Iter(df.get_column("scale").to_list())
+        .zip(df.get_column("color").to_list())
+        .pipe_into(dict)
+    )
+
+
+PALETTES: dict[str, list[str]] = get_palettes()
+# START MARKER
+Palettes = Literal[
+    "Alphabet",
+    "Alphabet_r",
+    "Cividis",
+    "Cividis_r",
+    "D3",
+    "D3_r",
+    "Dark24",
+    "Dark24_r",
+    "Edge",
+    "Edge_r",
+    "G10",
+    "G10_r",
+    "HSV",
+    "HSV_r",
+    "IceFire",
+    "IceFire_r",
+    "Inferno",
+    "Inferno_r",
+    "Light24",
+    "Light24_r",
+    "Magma",
+    "Magma_r",
+    "Plasma",
+    "Plasma_r",
+    "Plotly",
+    "Plotly3",
+    "Plotly3_r",
+    "Plotly_r",
+    "T10",
+    "T10_r",
+    "Turbo",
+    "Turbo_r",
+    "Twilight",
+    "Twilight_r",
+    "Viridis",
+    "Viridis_r",
+    "mrybm",
+    "mrybm_r",
+    "mygbm",
+    "mygbm_r",
+]
+# END MARKER
