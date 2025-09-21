e9cef9275cc9104ccc23477d2cc8fc1d3c3c5eca
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-09-22 01:43:11 +0200

    updated examples

--- tests/examples.py
+++ tests/examples.py
@@ -1,21 +1,30 @@
 from pathlib import Path
 
-import plotly.express as px
 import polars as pl
 
 import vizbuilder as vz
 
+SOURCE = Path().resolve().parent.joinpath("db", "prices").with_suffix(".parquet")
+
 
 def plot_graphs():
-    (
-        pl.scan_parquet(
-            Path().resolve().parent.joinpath("db", "prices").with_suffix(".parquet")
-        )
+    plotter = (
+        pl.scan_parquet(SOURCE)
         .sort("ticker", "date")
-        .pipe(vz.Displayer, group="ticker", template="plotly_dark")
-        .plot(px.line, x="date", y="close")
-        .show()
+        .drop_nulls()
+        .pipe(vz.Displayer, "ticker", "Plotly3", "plotly_dark")
     )
+    plotter.line(
+        x="date", y="close", title="Closing Prices of Different Tickers Over Time"
+    ).show()
+    plotter.bar(
+        y="stdev",
+        agg_expr=pl.median,
+        title="Median of Standard Deviation of Closing Prices by Ticker",
+    ).show()
+    plotter.histogram(x="stdev", title="Stdev Distribution").show()
+    plotter.box(y="stdev", title="Stdev Boxplot by Ticker", boxmode="group").show()
+    plotter.violin(y="stdev", title="Stdev Violin by Ticker", violinmode="group").show()
 
 
 if __name__ == "__main__":
